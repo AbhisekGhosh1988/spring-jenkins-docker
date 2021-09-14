61e140cb665f64776fd63ec36b0cb2f987199efc
AbhisekGhosh1988 <ghosh.abhisek1988@gmail.com>
2021-09-14 19:12:21 +0530

    first commit

---
+++ src/test/java/com/abhisek/demo/SpringJenkinsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhisek.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
